6603c198d73667d75cf4ab59dceb9f05a24df13e
Harshda Mhase <harshadamhase12@gmail.com>
2023-03-01 17:26:58 -0900

    ifstatement used

---
+++ cond1.java
@@ -0,0 +1,16 @@
+public class cond1
+{
+    int num=4;
+    public static void main(String args[])
+    {
+           int num=4;
+        if(num%2==0)
+        {
+        System.out.println("even number");
+         }
+         else
+         {
+        System.out.println("odd number");
+         }
+    }
+}
