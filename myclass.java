e94624ac7213f964d19a7addc180d9c7652b6d66
Harshda Mhase <harshadamhase12@gmail.com>
2023-03-01 16:48:38 -0900

    object created

---
+++ myclass.java
@@ -0,0 +1,13 @@
+public class myclass
+{
+    int x=10;
+    
+    public static void main(String args[])
+    {
+        myclass a1=new myclass();
+      myclass a2=new myclass();
+        System.out.println(a1.x);
+                System.out.println(a2.x);
+
+    }
+}
