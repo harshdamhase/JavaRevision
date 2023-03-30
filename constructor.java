fc2c91bcbff659ba55100661959deb5583828814
Harshda Mhase <harshadamhase12@gmail.com>
2023-03-30 12:02:12 -0900

    default construcotr example

---
+++ constructor.java
@@ -0,0 +1,13 @@
+
+public class constructor {
+//default constructor eg
+	constructor()
+	{
+		System.out.println("default contructor");
+	}
+	public static void main(String args[])
+	{
+		constructor c=new constructor();
+		
+	}
+}
